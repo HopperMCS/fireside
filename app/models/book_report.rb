8fe822779ff5ba29af98ad4464ac1e5f4461a187
M. Gage Morgan <gage@gages.blog>
2020-09-11 16:10:31 -0400

    Add validation to BookReport mdoel

--- app/models/book_report.rb
+++ app/models/book_report.rb
@@ -1,4 +1,8 @@
 class BookReport < ActiveRecord::Base
   belongs_to :user
   belongs_to :book
+  
+  validates :title, presence: true
+  validates :synopsis, presence: true
+  validates :review, presence: true
 end
