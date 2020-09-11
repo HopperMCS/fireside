853b397327aabe3920003177092ba524a7351735
M. Gage Morgan <gage@gages.blog>
2020-09-11 16:11:19 -0400

    Add validation to the Book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,9 @@
 class Book < ActiveRecord::Base
   has_many :book_reports
   belongs_to :user
+
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :release_year, presence: true
+  validates :purchase_link, presence: true
 end
